768baed44664ad7d0490a48be3471f45e452e3c8
Alain Martin <amartin@keytrol.org>
2017-01-21 16:56:18 +0100

    Do not change CMAKE_MODULE_PATH when it's not necessary

--- JUCE.cmake
+++ JUCE.cmake
@@ -22,5 +22,4 @@ set(JUCE.cmake_ROOT "${CMAKE_CURRENT_LIST_DIR}")
 
 set(JUCE_DIR "${JUCE.cmake_ROOT}/../JUCE" CACHE PATH "Path to JUCE")
 
-list(APPEND CMAKE_MODULE_PATH "${JUCE.cmake_ROOT}/cmake")
-include(CMakejucer)
+include("${JUCE.cmake_ROOT}/cmake/CMakejucer.cmake")
