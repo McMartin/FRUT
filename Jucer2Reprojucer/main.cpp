1e4b6ef0069d5d1aa50d1160ee56dc29bd6f9ff9
Alain Martin <amartin@keytrol.org>
2017-06-05 20:54:51 +0200

    Add list of supported commits

--- Jucer2Reprojucer/main.cpp
+++ Jucer2Reprojucer/main.cpp
@@ -15,6 +15,8 @@
 // You should have received a copy of the GNU General Public License
 // along with JUCE.cmake.  If not, see <http://www.gnu.org/licenses/>.
 
+#include "commits.hpp"
+
 #include "JuceHeader.h"
 
 #include <fstream>
