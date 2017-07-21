a71f1f749bbcb0f114d89a801d63f2f6d5707072
Alain Martin <amartin@keytrol.org>
2017-07-21 22:44:56 +0200

    Provide our own jucer_Headers.h instead of copying it from Projucer

---
+++ cmake/BinaryDataBuilder/Source/jucer_Headers.h
@@ -0,0 +1,24 @@
+// Copyright (c) 2017 Alain Martin
+//
+// This file is part of JUCE.cmake.
+//
+// JUCE.cmake is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// JUCE.cmake is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with JUCE.cmake.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "JuceHeader.h"
+
+#include "Utility/jucer_CodeHelpers.h"
+#include "Utility/jucer_FileHelpers.h"
+#include "Utility/jucer_MiscUtilities.h"
