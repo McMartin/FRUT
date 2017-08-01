0bf7d705c3bcb143d4060841eea1c3aea76a2489
Alain Martin <amartin@keytrol.org>
2017-08-01 23:06:08 +0200

    Introduce PListMerger helper executable
    It takes two plist contents and merges the second one into the first
    one, i.e. it only adds keys (and their associated values) from the
    second plist content if they don't already exist in the first one.

---
+++ cmake/PListMerger/main.cpp
@@ -0,0 +1,145 @@
+// Copyright (c) 2017 Alain Martin
+//
+// This file is part of JUCE.cmake.
+//
+// JUCE.cmake is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// JUCE.cmake is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with JUCE.cmake.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <juce_core/juce_core.h>
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+int main(int argc, char* argv[])
+{
+  if (argc != 3)
+  {
+    std::cerr << "usage: PListMerger"
+              << " <first-plist-content>"
+              << " <second-plist-content>" << std::endl;
+    return 1;
+  }
+
+  std::vector<std::string> args{argv, argv + argc};
+
+  juce::ScopedPointer<juce::XmlElement> firstPlistElement =
+    juce::XmlDocument::parse(args.at(1));
+  if (!firstPlistElement || !firstPlistElement->hasTagName("plist"))
+  {
+    std::cerr << "Invalid first plist content, expected <plist> element" << std::endl;
+    return 1;
+  }
+
+  auto firstDictElement = firstPlistElement->getChildByName("dict");
+  if (!firstDictElement)
+  {
+    std::cerr << "Invalid first plist content, expected <dict> element" << std::endl;
+    return 1;
+  }
+
+  std::vector<std::string> keysInFirstPlist;
+
+  for (auto childElement = firstDictElement->getFirstChildElement();
+       childElement != nullptr;
+       childElement = childElement->getNextElement())
+  {
+    if (childElement->getTagName() != "key" || childElement->getNumChildElements() != 1
+        || !childElement->getFirstChildElement()->isTextElement())
+    {
+      std::cerr << "Invalid first plist content, expected <key> element with only one "
+                   "text child element"
+                << std::endl;
+      return 1;
+    }
+
+    auto key = childElement->getFirstChildElement()->getText().toStdString();
+
+    if (std::find(keysInFirstPlist.begin(), keysInFirstPlist.end(), key)
+        != keysInFirstPlist.end())
+    {
+      std::cerr << "Invalid first plist content, duplicated key \"" << key << "\""
+                << std::endl;
+      return 1;
+    }
+
+    keysInFirstPlist.push_back(std::move(key));
+
+    childElement = childElement->getNextElement();
+    if (childElement == nullptr)
+    {
+      std::cerr << "Invalid first plist content, missing value associated with key \""
+                << key << "\"" << std::endl;
+      return 1;
+    }
+  }
+
+  juce::ScopedPointer<juce::XmlElement> secondPlistElement =
+    juce::XmlDocument::parse(args.at(2));
+  if (!secondPlistElement || !secondPlistElement->hasTagName("plist"))
+  {
+    std::cerr << "Invalid second plist content, expected <plist> tag" << std::endl;
+    return 1;
+  }
+
+  auto secondDictElement = secondPlistElement->getChildByName("dict");
+  if (!secondDictElement)
+  {
+    std::cerr << "Invalid second plist content, expected <dict> tag" << std::endl;
+    return 1;
+  }
+
+  for (auto childElement = secondDictElement->getFirstChildElement();
+       childElement != nullptr;
+       childElement = childElement->getNextElement())
+  {
+    if (childElement->getTagName() != "key" || childElement->getNumChildElements() != 1
+        || !childElement->getFirstChildElement()->isTextElement())
+    {
+      std::cerr << "Invalid second plist content, expected <key> element with only one "
+                   "text child element"
+                << std::endl;
+      return 1;
+    }
+
+    auto key = childElement->getFirstChildElement()->getText().toStdString();
+
+    const auto isKeyAlreadyInFirstPlist =
+      std::find(keysInFirstPlist.begin(), keysInFirstPlist.end(), key)
+      != keysInFirstPlist.end();
+
+    if (!isKeyAlreadyInFirstPlist)
+    {
+      firstDictElement->addChildElement(new juce::XmlElement(*childElement));
+    }
+
+    childElement = childElement->getNextElement();
+    if (childElement == nullptr)
+    {
+      std::cerr << "Invalid second plist content, missing value associated with key \""
+                << key << "\"" << std::endl;
+      return 1;
+    }
+
+    if (!isKeyAlreadyInFirstPlist)
+    {
+      firstDictElement->addChildElement(new juce::XmlElement(*childElement));
+    }
+  }
+
+  std::cout << firstPlistElement->createDocument(juce::String{}, false, false);
+
+  return 0;
+}
