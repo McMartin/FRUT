594af48ff452a2996f0e2aa70fb258629f69f61d
Alain Martin <amartin@keytrol.org>
2017-09-02 16:24:21 +0200

    Use correct variable
    It doesn't make a big difference now, since we are only supporting the
    "Visual Studio 2015" and "Visual Studio 2013" export target, but it will
    when we support the "Code::Blocks (Windows)" one.

--- cmake/Reprojucer.cmake
+++ cmake/Reprojucer.cmake
@@ -2127,7 +2127,7 @@ function(__set_common_target_properties target)
       )
     endif()
 
-  elseif(WIN32)
+  elseif(MSVC)
     foreach(config ${JUCER_PROJECT_CONFIGURATIONS})
       if(${JUCER_CONFIGURATION_IS_DEBUG_${config}})
         target_compile_definitions(${target} PRIVATE
