8b9608249c3a59eb9188920b228e672c3e41ebc4
Alain Martin <amartin@keytrol.org>
2017-11-19 00:49:09 +0100

    Use a better way of requiring CMake version 3.4
    According to CMake's documentation, cmake_minimum_required() is meant
    to be used at the beginning of CMakeLists.txt files.

--- cmake/Reprojucer.cmake
+++ cmake/Reprojucer.cmake
@@ -15,7 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with FRUT.  If not, see <http://www.gnu.org/licenses/>.
 
-cmake_minimum_required(VERSION 3.4)
+if(CMAKE_VERSION VERSION_LESS 3.4)
+  message(FATAL_ERROR "Reprojucer requires at least CMake version 3.4")
+endif()
 
 if(CMAKE_VERSION VERSION_LESS 3.5)
   include(CMakeParseArguments)
