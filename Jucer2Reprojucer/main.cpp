78f3a50023e309a3f1a30b99f01c1cdd2c7d7ce3
Alain Martin <amartin@keytrol.org>
2018-09-19 23:42:01 +0200

    Style: Add empty lines to improve readability

--- Jucer2Reprojucer/main.cpp
+++ Jucer2Reprojucer/main.cpp
@@ -560,14 +560,18 @@ int main(int argc, char* argv[])
                                          "ENABLE_INTERAPP_AUDIO", false);
         }
       }
+
       convertSetting(jucerProject, "pluginName", "PLUGIN_NAME", {});
       convertSetting(jucerProject, "pluginDesc", "PLUGIN_DESCRIPTION", {});
+
       convertSetting(jucerProject, "pluginManufacturer", "PLUGIN_MANUFACTURER", {});
       convertSetting(jucerProject, "pluginManufacturerCode", "PLUGIN_MANUFACTURER_CODE",
                      {});
       convertSetting(jucerProject, "pluginCode", "PLUGIN_CODE", {});
+
       convertSetting(jucerProject, "pluginChannelConfigs",
                      "PLUGIN_CHANNEL_CONFIGURATIONS", {});
+
       if (jucerVersionAsTuple < Version{5, 3, 1})
       {
         convertOnOffSettingWithDefault(jucerProject, "pluginIsSynth", "PLUGIN_IS_A_SYNTH",
@@ -581,12 +585,14 @@ int main(int argc, char* argv[])
         convertOnOffSettingWithDefault(jucerProject, "pluginEditorRequiresKeys",
                                        "KEY_FOCUS", false);
       }
+
       if (jucerVersionAsTuple >= Version{5, 3, 1})
       {
         convertSetting(jucerProject, "aaxIdentifier", "PLUGIN_AAX_IDENTIFIER", {});
       }
       convertSetting(jucerProject, "pluginAUExportPrefix", "PLUGIN_AU_EXPORT_PREFIX", {});
       convertSetting(jucerProject, "pluginAUMainType", "PLUGIN_AU_MAIN_TYPE", {});
+
       convertSetting(jucerProject, "pluginVSTCategory",
                      jucerVersionAsTuple > Version{5, 3, 0} ? "PLUGIN_VST_CATEGORY"
                                                             : "VST_CATEGORY",
@@ -611,10 +617,12 @@ int main(int argc, char* argv[])
       }
       convertSetting(jucerProject, "pluginRTASCategory", "PLUGIN_RTAS_CATEGORY", {});
       convertSetting(jucerProject, "pluginAAXCategory", "PLUGIN_AAX_CATEGORY", {});
+
       if (jucerVersionAsTuple < Version{5, 3, 1})
       {
         convertSetting(jucerProject, "aaxIdentifier", "PLUGIN_AAX_IDENTIFIER", {});
       }
+
       wLn(")");
       wLn();
     }
