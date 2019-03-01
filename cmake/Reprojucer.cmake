McMartin/FRUT

commit 7289e4ac129ec188f1554f71cbf1e4b8d824223b

Reduce the length of _FRUT_generate_AppConfig_header()
... by extracting one functions from it.

cmake/Reprojucer.cmake
@@ -3033,49 +3033,14 @@ function(_FRUT_generate_AppConfig_header)
     endif()
 
     if(DEFINED JUCER_PLUGIN_VST3_CATEGORY)
-      set(categories "${JUCER_PLUGIN_VST3_CATEGORY}")
-
-      # See getVST3CategoryStringFromSelection()
-      # in JUCE/extras/Projucer/Source/Project/jucer_Project.cpp
-      if(NOT "Fx" IN_LIST categories AND NOT "Instrument" IN_LIST categories)
-        if(JUCER_PLUGIN_IS_A_SYNTH)
-          list(INSERT categories 0 "Instrument")
-        else()
-          list(INSERT categories 0 "Fx")
-        endif()
-      else()
-        if("Instrument" IN_LIST categories)
-          list(FIND categories "Instrument" Instrument_index)
-          list(REMOVE_AT categories ${Instrument_index})
-          list(INSERT categories 0 "Instrument")
-        endif()
-        if("Fx" IN_LIST categories)
-          list(FIND categories "Fx" Fx_index)
-          list(REMOVE_AT categories ${Fx_index})
-          list(INSERT categories 0 "Fx")
-        endif()
-      endif()
-
-      list(LENGTH categories categories_count)
-      if(categories_count EQUAL 1)
-        set(vst3_category "${categories}")
-      else()
-        list(GET categories 0 first_category)
-        set(vst3_category "${first_category}")
-        math(EXPR categories_max "${categories_count} - 1")
-        foreach(index RANGE 1 ${categories_max})
-          list(GET categories ${index} category)
-          string(APPEND vst3_category "|${category}")
-        endforeach()
-      endif()
+      _FRUT_compute_vst3_category(Vst3Category_value)
     else()
       if(JUCER_PLUGIN_IS_A_SYNTH)
-        set(vst3_category "Instrument|Synth")
+        set(Vst3Category_value "Instrument|Synth")
       else()
-        set(vst3_category "Fx")
+        set(Vst3Category_value "Fx")
       endif()
     endif()
-    set(Vst3Category_value "\"${vst3_category}\"")
 
     if(NOT DEFINED JUCER_PLUGIN_AU_MAIN_TYPE)
       if(JUCER_MIDI_EFFECT_PLUGIN)
@@ -4537,6 +4502,49 @@ function(_FRUT_get_au_main_type_code out_value)
   endif()
 
   set(${out_value} "${code}" PARENT_SCOPE)
+
+endfunction()
+
+
+function(_FRUT_compute_vst3_category out_var)
+
+  set(categories "${JUCER_PLUGIN_VST3_CATEGORY}")
+
+  # See getVST3CategoryStringFromSelection()
+  # in JUCE/extras/Projucer/Source/Project/jucer_Project.cpp
+  if(NOT "Fx" IN_LIST categories AND NOT "Instrument" IN_LIST categories)
+    if(JUCER_PLUGIN_IS_A_SYNTH)
+      list(INSERT categories 0 "Instrument")
+    else()
+      list(INSERT categories 0 "Fx")
+    endif()
+  else()
+    if("Instrument" IN_LIST categories)
+      list(FIND categories "Instrument" Instrument_index)
+      list(REMOVE_AT categories ${Instrument_index})
+      list(INSERT categories 0 "Instrument")
+    endif()
+    if("Fx" IN_LIST categories)
+      list(FIND categories "Fx" Fx_index)
+      list(REMOVE_AT categories ${Fx_index})
+      list(INSERT categories 0 "Fx")
+    endif()
+  endif()
+
+  list(LENGTH categories categories_count)
+  if(categories_count EQUAL 1)
+    set(vst3_category "${categories}")
+  else()
+    list(GET categories 0 first_category)
+    set(vst3_category "${first_category}")
+    math(EXPR categories_max "${categories_count} - 1")
+    foreach(index RANGE 1 ${categories_max})
+      list(GET categories ${index} category)
+      string(APPEND vst3_category "|${category}")
+    endforeach()
+  endif()
+
+  set(${out_var} "${vst3_category}" PARENT_SCOPE)
 
 endfunction()
 

commit 938205ec75c321bc9430ca100f967544b893b6f7

Fix missing quotes around the value of JucePlugin_Vst3Category
This was broken in 7289e4ac129ec188f1554f71cbf1e4b8d824223b.

cmake/Reprojucer.cmake
@@ -2801,14 +2801,15 @@ function(_FRUT_generate_AppConfig_header)
     endif()
 
     if(DEFINED JUCER_PLUGIN_VST3_CATEGORY)
-      _FRUT_compute_vst3_category(Vst3Category_value)
+      _FRUT_compute_vst3_category(vst3_category)
     else()
       if(JUCER_PLUGIN_IS_A_SYNTH)
-        set(Vst3Category_value "Instrument|Synth")
+        set(vst3_category "Instrument|Synth")
       else()
-        set(Vst3Category_value "Fx")
+        set(vst3_category "Fx")
       endif()
     endif()
+    set(Vst3Category_value "\"${vst3_category}\"")
 
     if(NOT DEFINED JUCER_PLUGIN_AU_MAIN_TYPE)
       if(JUCER_MIDI_EFFECT_PLUGIN)