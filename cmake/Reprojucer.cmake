4d1d649c02703c152d148d12a4276fdd268d9ba0
Alain Martin <amartin@keytrol.org>
2019-04-06 20:12:04 +0200

    Simplify condition to check whether a variable is empty

--- cmake/Reprojucer.cmake
+++ cmake/Reprojucer.cmake
@@ -1642,7 +1642,7 @@ function(jucer_project_end)
       endif()
     endforeach()
 
-    if(DEFINED icon_filename AND NOT icon_filename STREQUAL "")
+    if(icon_filename)
       string(CONCAT resources_rc_icon_settings
         "\n"
         "\nIDI_ICON1 ICON DISCARDABLE \"${icon_filename}\""
