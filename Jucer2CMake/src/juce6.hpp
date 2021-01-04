b84b269486b38431b8981deef95ac959ec3d4c01
Alain Martin <amartin@keytrol.org>
2021-01-04 22:21:29 +0100

    Introduce 'juce6' mode
    The purpose of this mode is to generate CMakeLists.txt files that use
    JUCE 6's CMake support.

---
+++ Jucer2CMake/src/juce6.hpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2021  Alain Martin
+//
+// This file is part of FRUT.
+//
+// FRUT is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// FRUT is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with FRUT.  If not, see <http://www.gnu.org/licenses/>.
+
+#pragma once
+
+#include "utils.hpp"
+
+#include "juce_core.hpp"
+
+
+namespace Jucer2CMake
+{
+
+inline void writeJuce6CMakeLists(const Arguments&, const juce::XmlElement&,
+                                 juce::MemoryOutputStream&)
+{
+}
+
+} // namespace Jucer2CMake
