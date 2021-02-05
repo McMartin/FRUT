1d4e099047ee6d9afa1eea6ee3529234fa2dc7ff
Alain Martin <amartin@keytrol.org>
2021-02-05 07:13:24 +0100

    juce6: Support the "Plugin Code" project setting

--- Jucer2CMake/src/juce6.hpp
+++ Jucer2CMake/src/juce6.hpp
@@ -145,6 +145,7 @@ inline void writeJuce6CMakeLists(const Arguments&, const juce::XmlElement& jucer
 
       writeProjectSettingIfDefined("pluginName", "PLUGIN_NAME");
       writeProjectSettingIfDefined("pluginManufacturerCode", "PLUGIN_MANUFACTURER_CODE");
+      writeProjectSettingIfDefined("pluginCode", "PLUGIN_CODE");
     }
 
     wLn(")");
